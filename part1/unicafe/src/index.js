6359c6808515cf4d29cbef955847f6894c99c7fc
Tamanji Che <cheambetamanji@gmail.com>
2020-06-01 22:47:45 +0300

    conditional rendering of statistics component implemented, code refactored into required components

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const Header = ({ text }) => {
+  return (
+    <div>
+      <h1> {text} </h1>
+    </div>
+  );
+};
+
+const Button = ({ text, handleClick }) => {
+  return (
+    <>
+      <button onClick={handleClick}> {text} </button>
+    </>
+  );
+};
+
+const Statistic = ({ stats, count }) => {
+  return (
+    <>
+      <p> {stats}: {count} </p>
+    </>
+  );
+};
+
+const Statistics = ({ feedback }) => {
+  const [good, neutral, bad] = feedback;
+  const totalCount = good + neutral + bad;
+  const ave = (good - bad) / totalCount;
+  const percent = (good / totalCount) * 100;
+
+  if (totalCount === 0) {
+    return (
+      <>
+        <p> No feedback, click a button above to give feedback </p>
+      </>
+    );
+  }
+  return (
+    <>
+      <Statistic stats="Good" count={good} />
+      <Statistic stats="Neutral" count={neutral} />
+      <Statistic stats="Bad" count={bad} />
+      <Statistic stats="All" count={totalCount} />
+      <Statistic stats="Average" count={ave} />
+      <Statistic stats="Positive" count={`${percent} %`} />
+    </>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => () => setGood(good + 1);
+  const handleNeutral = () => () => setNeutral(neutral + 1);
+  const handleBad = () => () => setBad(bad + 1);
+
+  const feedback = [good, neutral, bad];
+
+  return (
+    <div>
+      <Header text="Give Feedback" />
+      <Button text="Good" handleClick={handleGood()} />
+      <Button text="Neutral" handleClick={handleNeutral()} />
+      <Button text="Bad" handleClick={handleBad()} />
+      <Header text="Statistics" />
+      <Statistics feedback={feedback} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
