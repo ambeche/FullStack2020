dcb8e18460e2e82ad5f46810bbc155c467bf06bd
Tamanji Che <cheambetamanji@gmail.com>
2020-06-02 21:29:13 +0300

    final version of app; most voted anecdote displayed

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Header = ({ heading }) => {
+  return (
+    <>
+      <h1> {heading} </h1>
+    </>
+  );
+};
+
 const Button = ({ text, handleClick }) => {
   return (
     <>
@@ -9,8 +17,33 @@ const Button = ({ text, handleClick }) => {
   );
 };
 
+const MostVoted = ({ statesProps }) => {
+  const [votes, anecdotes] = statesProps;
+
+  if (votes.reduce((sum, e) => sum + e, 0) > 0) {
+    const maxi = Math.max(...votes);
+    console.log("votes:", votes);
+    return (
+      <>
+        <p>
+          {anecdotes[votes.indexOf(maxi)]} <br />
+          Has {votes[votes.indexOf(maxi)]} votes
+        </p>
+      </>
+    );
+  }
+  return (
+    <>
+      <p>No votes yet, click the button above to vote.</p>
+    </>
+  );
+};
+
 const App = ({ anecdotes }) => {
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState(
+    Math.floor(Math.random() * anecdotes.length)
+  );
+  
   const [votes, setVotes] = useState(
     Array.apply(null, new Array(anecdotes.length)).map(
       Number.prototype.valueOf,
@@ -26,16 +59,22 @@ const App = ({ anecdotes }) => {
     const votesClone = [...votes];
     votesClone[selected] += 1;
     setVotes(votesClone);
-    console.log(votes);
+    console.log(votes[selected]);
   };
+
+  const statesProps = [votes, anecdotes];
 
   return (
     <div>
+      <Header heading="Anecdote of The Day" />
       <p>
-        {anecdotes[selected]} Has {votes[selected]} votes
+        {anecdotes[selected]} <br />
+        Has {votes[selected]} votes
       </p>
       <Button text="Vote" handleClick={handleVotes()} />
       <Button text="Next Anecdote" handleClick={handleRanSelection()} />
+      <Header heading="Anecdote With The Most Votes" />
+      <MostVoted statesProps={statesProps} />
     </div>
   );
 };
