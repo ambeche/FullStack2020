0269578300db7d157874140c26582c3dcad13e80
Tamanji Che <cheambetamanji@gmail.com>
2020-06-09 01:05:36 +0300

    Exercise 2.14: fetching and rendering weather reports for capital cities

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -7,46 +7,60 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchText, setSearchText] = useState("");
   const [clickedCountry, setClickedCountry] = useState([]);
-  const [clicked, setClicked] = useState(false);
+  const [weather, setWeather] = useState({});
+  const [query, setQuery] = useState("Espoo");
 
   useEffect(() => {
-    const getCountries = async () => {
+    const getData = async () => {
       try {
         const res = await axios.get("https://restcountries.eu/rest/v2/all");
+
+        const api_key = process.env.REACT_APP_API_KEY;
+        console.log("apiKey", api_key);
+
+        const weather = await axios.get(` http://api.weatherstack.com/current?access_key=${api_key}&query=${query}`);
+
         console.log("fetched data", res.data);
         setCountries(res.data);
+        setWeather(weather.data);
         console.log("res", res);
       } catch (e) {
         console.error(e.message);
       }
     };
-    getCountries();
-  }, []);
+    getData();
+  }, [query]);
 
   const filteredCountries = countries.filter((p) =>
     p.name.toLowerCase().includes(searchText.toLowerCase())
   );
 
-  const countriesToShow =
-  clicked ? clickedCountry : searchText.length !== 0 ? filteredCountries : [];
+  const countriesToShow = clickedCountry.length > 0
+    ? clickedCountry
+    : searchText.length !== 0
+    ? filteredCountries
+    : [];
 
   const searchCountry = (e) => {
     console.log("searchText", e.target.value);
     setSearchText(e.target.value);
-    setClicked(false)
+    setClickedCountry([])
   };
 
   const showCountry = (country) => {
-    const toShow = countriesToShow.filter((p) => p.name.toLowerCase().includes(country.name.toLowerCase()));
-    setClicked(true)
-    setClickedCountry(toShow)
-    console.log('clicked country', toShow); 
-  }
+    const toShow = countriesToShow.filter((p) =>
+      p.name.toLowerCase().includes(country.name.toLowerCase())
+    );
+    setClickedCountry(toShow);
+    console.log("clicked country", toShow);
+  };
+
+  const provideQuery = (country) => setQuery(country.capital)
 
   return (
     <div>
       <Filter searchCountry={searchCountry} searchText={searchText} />
-      <Countries countriesToShow={countriesToShow} showCountry={showCountry} />
+      <Countries data={[countriesToShow, showCountry, provideQuery, weather]} />
     </div>
   );
 };
--- part2/data-for-countries/src/components/Countries.js
+++ part2/data-for-countries/src/components/Countries.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Languages = ({ languages }) => {
   return (
@@ -12,7 +12,33 @@ const Languages = ({ languages }) => {
   );
 };
 
-const Country = ({ country, countriesToShow, showCountry }) => {
+const Weather = ({ weather }) => {
+  return (
+    <div>
+      <strong> Temperature: </strong> {weather.temperature} Celsius
+      <div>
+        <img
+          src={weather.weather_icons[0]}
+          alt={weather.weather_descriptions[0]}
+          height="100"
+          width="100"
+        />
+      </div>
+      <div>{weather.weather_descriptions[0]}</div>
+      <strong>Wind: </strong> {weather.wind_speed} mph, direction{" "}
+      {weather.wind_dir}
+    </div>
+  );
+};
+
+const Country = ({
+  country,
+  countriesToShow,
+  showCountry,
+  provideQuery,
+  weather,
+}) => {
+  useEffect(() => provideQuery(country));
 
   if (countriesToShow.length === 1) {
     return (
@@ -28,19 +54,23 @@ const Country = ({ country, countriesToShow, showCountry }) => {
           height="100"
           width="100"
         />
+        <h3>Weather in {country.capital}</h3>
+        <Weather weather={weather} />
       </div>
     );
   } else if (countriesToShow.length <= 10) {
     return (
       <div>
         {country.name}
-        <button onClick={ () => showCountry(country)}>Show</button>
+        <button onClick={() => showCountry(country)}>Show</button>
       </div>
     );
   }
 };
 
-const Countries = ({ countriesToShow, showCountry }) => {
+const Countries = ({ data }) => {
+  const [countriesToShow, showCountry, provideQuery, weather] = data;
+
   if (countriesToShow.length > 10) {
     return <div>Too many matches, specify another filter.</div>;
   }
@@ -53,6 +83,8 @@ const Countries = ({ countriesToShow, showCountry }) => {
           country={country}
           countriesToShow={countriesToShow}
           showCountry={showCountry}
+          provideQuery={provideQuery}
+          weather={weather.current}
         />
       ))}
     </div>
